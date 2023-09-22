2c1d334a8e73dc833ff2442fae93f1991f6cc7a9
samad aderinto <81313734+samadaderinto@users.noreply.github.com>
2023-09-22 08:15:19 +0100

    completed app using api audio

--- src/App.js
+++ src/App.js
@@ -1,48 +1,97 @@
-import './App.scss';
-import React,{ useEffect } from 'react';
-import Form from './components/Form/form';
-import Header from './components/Header/header';
-import Results from './components/Results/results';
+import {React, useEffect, useState} from 'react'
 import apiaudio from 'apiaudio'
-import KEY_API_AUDIO from './key';
-
-function App() {
+import  "./App.css"
 
 
-    const runScript = async() => {
-        apiaudio.configure({apiKey: KEY_API_AUDIO});
 
-        try {
+const App = ()=> {
+  const [check, setCheck] = useState(0)
+    
+  
+  const [formData, setFormData] = useState({
+    text: "",
+    speaker: "",
+    template: ""
+  })
 
-            const createScript = await apiaudio
-                .script
-                .create({scriptText: "samad", scriptId: "samad", projectName: "text2speech", moduleName: "samad", scriptName: "samad"});
+  const handleChange = (e)=> {
+    const { name, value } = e.target;
 
-            const createSpeech = await apiaudio
-                .Speech
-                .create({scriptId: "samad", voice: "Sonia", speed: 100});
-
-            const createAudio = await apiaudio
-                .mastering
-                .create({scriptId: "samad", soundTemplate: "qq"});
-
-
-        } catch (error) {
-            alert(error)
-        }
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    console.log(formData)
+  };
+  
+  const speaker = ["ryan", "libby", "aria", "guy", "david", "elvira","matthias", "ingrid", "pauline", "henri", "alena", "dmitry"]
+  const speakers = speaker.map(element => {
+    return (
+    <option key = {element} value={element}>{element.toUpperCase()}</option>)})
+  const template = ["copacabana", "breakingnews", "hautecuisine", "openup", "curtaincall", "fairytale", "hotwheels"]
+  const templates = template.map(element => {
+    return (
+    <option key={element} value={element}>{element.toUpperCase()}</option>)})
+  
+    async function helloWorld() {
+      if (check ===0) {
+        apiaudio.configure({ apiKey: "1bbba608c42f4c1388e3a2144a2d9720" });
+        return
+      }
+      
+      try {
+        const script = await apiaudio.Script.create({ scriptText: `
+       SOUNDSEGMENT::INTRO
+       SECTIONNAME::INTRO 
+       Hello world. Welcome to API dot audio.
+       SOUNDSEGMENT::MAIN
+       SECTIONNAME::MAIN
+       ${formData.text} <break time = '1s'/>
+       SOUNDSEGMENT::OUTRO 
+       SECTIONNAME::OUTRO
+       We're excited to hear your script!
+       `, scriptName: "hello", projectName: "hello", moduleName: "hello" });
+        console.log(script);
+    
+        const speech = await apiaudio.Speech.create({ scriptId: script["scriptId"], voice: `${formData.speaker}` });
+        console.log(speech); 
+    
+        const template = `${formData.template}`;
+        const mastering = await apiaudio.Mastering.create({ scriptId: script["scriptId"], soundTemplate: template });
+        console.log(mastering);
+        
+      } catch (e) {
+        console.error(e);
+      }
     }
 
-    useEffect(runScript(),[])
-    return (
-        <div className="App">
-            <Header/>
-            <main className='App-main'>
-                <Form/>
-                <Results/>
-            </main>
+    useEffect((helloWorld()), [check])
+  
+  
+  return (
+    <div className = "all">
+      <h1>BUILD AUDIO IN SECONDS</h1>
+      <div className="text">
+        <span>Type Anything and choose your speaker</span>
+        <input type="text" name="text" placeholder="kindly enter your text here"  value={formData.text} onChange={handleChange}/>
+      </div>
+      <div className="chs-template">
+        <select name="speaker" value={formData.speaker} onChange={handleChange}>
+          {speakers}
+        </select>
+      </div>
+      <div className="chs-template">
+        <span>Choose a template and create your audio</span>
+        <select name="template"  value={formData.template} onChange={handleChange}>
+          {templates}
+        </select>
+      </div>
+      <button>Generate Audio</button>
+            
+      <audio  src="./static/music/foo.mp3" controls autoPlay/>
+    </div>
 
-        </div>
-    );
+  )
 }
 
 export default App;
